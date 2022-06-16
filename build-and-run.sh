0b190b9b912d02be342c4c2e1b7df1e454cb905f
Edwin Pers <pers.edwin@gmail.com>
2022-06-16 14:14:51 +0000

    static binary build mode

--- build-and-run.sh
+++ build-and-run.sh
@@ -2,6 +2,7 @@
 set -exu
 
 cd ./src
-go build -a -v -o ../build .
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -v -o ../build .
 cd ../build
+file ./netlink-api | tr , '\n'
 sudo ./netlink-api
