8bc2bf43f2ab82bb2bf52b00a1ab24f923f18a2d
Edwin Pers <pers.edwin@gmail.com>
2022-06-19 12:07:32 +0000

    allow specification of listen address:port, handle sigint/sigterm cleanly

--- build-and-run.sh
+++ build-and-run.sh
@@ -5,4 +5,6 @@ cd ./src
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -v -o ../build .
 cd ../build
 file ./netlink-api | tr , '\n'
-sudo ./netlink-api
+
+# defaults to localhost:4821 if not provided
+sudo LISTEN=:4821 ./netlink-api
--- src/main.go
+++ src/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	routingv1 "github.com/plonkfw/netlink-api/routing/v1"
@@ -17,6 +21,27 @@ func init() {
 
 // Setup http listener and routes
 func main() {
+	sigChannel := make(chan os.Signal, 1)
+	signal.Notify(sigChannel, os.Interrupt, os.Kill, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		for signal := range sigChannel {
+			cleanup(signal)
+		}
+	}()
+
+	listen := os.Getenv("LISTEN")
+	if listen == "" {
+		listen = "localhost:4829"
+	}
 	routerv1 := routingv1.NewAPIRouter()
-	utilsv1.Log.Fatal().Err(http.ListenAndServe(":4821", routerv1))
+	utilsv1.Log.Fatal().Err(http.ListenAndServe(listen, routerv1))
+}
+
+func cleanup(signal os.Signal) {
+	msg := fmt.Sprintf("Caught %v signal, exiting", signal)
+	utilsv1.Log.Info().Msg(msg)
+	t := time.Now()
+	// WHY isn't this format specification just yyyy-MM-dd etc
+	utilsv1.Log.Info().Msg("Logs end at " + t.UTC().Format("2006-01-02 15:04:05") + " UTC")
+	os.Exit(0)
 }
